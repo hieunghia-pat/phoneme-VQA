cce0ac8982fdb4a028ca4a6593f8f7e69dd17d2e
Nguyễn Thái Nhật An <166610549+Beoconcuame@users.noreply.github.com>
2024-11-22 12:48:01 +0700

    Update vocab_builder.py

--- core/tokenizer/modules/vocab_builder.py
+++ core/tokenizer/modules/vocab_builder.py
@@ -9,12 +9,26 @@ import string
 from .word_processing import is_Vietnamese
 
 class VocabBuilder:
-    def __init__(self, annotation_paths: list[str]):
+    def __init__(self, annotation_paths: list[str] = None):
         self.annotation_paths = annotation_paths
-        self.vocab = {'onset': {'none': 0, '<_>': 1, '<pad>':2, '<bos>':3, '<eos>':4}, 'rhyme': {'none': 0}, 'tone': {'none': 0},'none':{'none': 0}}
+
+        self.vocab = {
+            'onset': {'none': 0,'<_>': 1},
+            'rhyme': {'none': 0},
+            'tone': {'none': 0}
+        }
+        
+        # Thêm token đặc biệt <pad>, <bos>, và <eos> vào từ điển onset
+        if '<pad>' not in self.vocab['onset']:
+            self.vocab['onset']['<pad>'] = len(self.vocab['onset'])
+        if '<bos>' not in self.vocab['onset']:
+            self.vocab['onset']['<bos>'] = len(self.vocab['onset'])
+        if '<eos>' not in self.vocab['onset']:
+            self.vocab['onset']['<eos>'] = len(self.vocab['onset'])
+        
         self.word_sources = {'onset': {}, 'rhyme': {}, 'tone': {}}
         self.text_sources = {'rhyme': {}}
-        self.create_vocab()
+        self.word_counts = self.create_vocab()
 
     def create_vocab(self) -> dict[str, int]:
         for annotation_path in self.annotation_paths:
@@ -24,8 +38,8 @@ class VocabBuilder:
             annotations = json_data.get("annotations", [])
 
             for ann in annotations:
-                # Collect words from answers
-                for field in ["answers"]:
+                # Collect words from questions and answers
+                for field in ["question", "answers"]:
                     if field in ann:
                         text = ann[field] if isinstance(ann[field], str) else ann[field][0]
                         for word in text.split():
@@ -81,12 +95,17 @@ class VocabBuilder:
                                     elif char in self.word_sources['onset'] and word not in self.word_sources['onset'][char]:
                                         self.word_sources['onset'][char].append(word)
 
-                                # Check for missing characters in onset from string.printable
-                                for printable_char in string.ascii_lowercase + string.digits + string.punctuation + ' ' + '<_>':
+                                # Đảm bảo không thêm dấu " " vào bộ từ vựng vì đã có "<_>" đại diện cho khoảng trắng
+                                for printable_char in string.ascii_lowercase + string.digits + string.punctuation:
                                     if printable_char not in self.vocab['onset']:
                                         self.vocab['onset'][printable_char] = len(self.vocab['onset'])
                                         self.word_sources['onset'][printable_char] = []
+                                # Đảm bảo "<_>" đã có trong từ điển
+                                if '<_>' not in self.vocab['onset']:
+                                    self.vocab['onset']['<_>'] = len(self.vocab['onset'])
+                                    self.word_sources['onset']['<_>'] = []
 
+        return self.vocab
 
     def check_vocab(self):
         # Print the vocabulary and its size
